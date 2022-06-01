4ef23058ef1e892ad23ae82ab5ca267b9e425dd5
amy-upthagrove <amyupthagrove@gmail.com>
2022-06-01 11:02:52 -0500

    add metadescription to blog post list page

--- frontend/website/pages/blog/index.js
+++ frontend/website/pages/blog/index.js
@@ -2,11 +2,17 @@ import { Component } from "react"
 
 import Header from "../../components/header.js"
 import Footer from "../../components/footer.js"
+import headMetadata from "../../components/headMetadata.js"
+import HeadMetadata from "../../components/headMetadata.js"
 
 export default class extends Component {
   render () {
     return (
       <div className="layout-wrapper">
+        <HeadMetadata
+          title="Blog Posts || Field Guide"
+          metaDescription="List of all blog posts published on the Field Guide for the Sound Mind blog."
+          />
         <Header />
         <div className="blog-posts-container">
           <h1>Blog posts</h1>
